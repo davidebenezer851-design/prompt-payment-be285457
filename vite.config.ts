180d9da335d896826b135417add1bc65fe2dd701
unknown <davidebenezer851@gmail.com>
2026-06-25 18:17:42 +0100

    universal spa fallback deployment fix

--- vite.config.ts
+++ vite.config.ts
@@ -6,10 +6,4 @@
 // You can pass additional config via defineConfig({ vite: { ... }, etc... }) if needed.
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
-export default defineConfig({
-  tanstackStart: {
-    server: {
-      entry: "server",
-    },
-  },
-});
+export default defineConfig();
